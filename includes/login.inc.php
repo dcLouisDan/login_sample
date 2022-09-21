9616e3959af4bedef285ecbd2eb1d22cd43d0493
Dan Louis Dela Cruz <louisdc002@gmail.com>
2022-09-21 12:26:34 +0800

    Fix more bracket errors

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,7 +1,7 @@
 <?php
 include('class-autoload.inc.php');
 
-if ($_SERVER['REQUEST_METHOD'] == ['POST']) {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
   $email = $_POST['email'];
   $password = $_POST['password'];
 
